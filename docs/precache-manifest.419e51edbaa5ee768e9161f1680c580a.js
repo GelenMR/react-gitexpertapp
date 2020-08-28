475f248cdafc674a2d019100ff807ebd1ea2e75f
GelenMR <gelen.montilla.r@gmail.com>
2020-08-28 12:30:35 -0400

    Carpeta Build Renombrada a Docs

---
+++ docs/precache-manifest.419e51edbaa5ee768e9161f1680c580a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "559de87c5b0b45ae2276b66ed607248d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d0077dd2f868a709ffb",
+    "url": "/static/css/main.2b63fab8.chunk.css"
+  },
+  {
+    "revision": "dcf09d6ede3b15119fcb",
+    "url": "/static/js/2.a16041ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a16041ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d0077dd2f868a709ffb",
+    "url": "/static/js/main.6060590a.chunk.js"
+  },
+  {
+    "revision": "9d3929294d677b1b9ff6",
+    "url": "/static/js/runtime-main.aa9df6a3.js"
+  }
+]);
